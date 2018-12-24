e7354b0faca1fb4709406a2425dc1488e52958f8
Vignesh Vaidyanathan <vigneshv01@gmail.com>
2018-12-24 17:43:46 +0530

    added new screen

---
+++ app/src/main/java/mobile/opencrm/ViewOrders.kt
@@ -0,0 +1,12 @@
+package mobile.opencrm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewOrders : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_orders)
+    }
+}
